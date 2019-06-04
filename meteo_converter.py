35956e5be8edc3c634434f08441bd1eead835d61
simone <bonsisimone94@gmail.com>
2019-06-04 18:59:31 +0200

    Aggiunto if per sistemare la data

--- meteo_converter.py
+++ meteo_converter.py
@@ -15,4 +15,19 @@ for nomeMese in mesi:
             header = next(lettore)
             writer = csv.writer(newMese, delimiter=";") 
             writer.writerow(header[:]) #Copio l'header del file di origine nel nuovo file
+            dati = [(linea[:]) for linea in lettore]
+            for riga in dati:
+                str = riga[1]   #Prendo solo gli elementi della colonna Data
+            if "/" in str:  #Se la data contiene lo / significa che è scritta nel formato sbagliato
+                #print (f"{riga[0]}") #visualizzo la data nel formato errato
+                #print(str[6:11]+ "-" + str[3:5] + "-" + str[0:2]) #la ristrutturo nel formato corretto
+                print(riga[1])
+                print(str[6:11])
+                print(str[3:5])
+                print(str[0:2])
+                #newData = (str[6:11] + "-" + str[3:5] + "-" + str[0:2])
+                #riga[1] = newData
+                #print(riga[1])
+                #writer.writerow(riga[:]) #In questo caso la riga è formattata come vogliamo e la copiamo nel nuovo csv
+
             
