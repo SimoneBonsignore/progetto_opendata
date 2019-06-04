ba2ade5f66a3dfc3fd9b31c7936268d8160a17ad
simone <bonsisimone94@gmail.com>
2019-06-04 16:54:34 +0200

    Aggiunta stringa corretta

--- Test.py
+++ Test.py
@@ -12,7 +12,10 @@ with open("./dati/sinistri2018.csv", newline="", encoding="ISO-8859-1") as sinis
             str = riga[0]   #Prendo solo gli elementi della colonna Data
             if "/" in str:  #Se la data contiene lo / significa che è scritta nel formato sbagliato
                 print (f"{riga[0]}") #visualizzo la data nel formato errato
-                print(str[6:11]+ "-" + str[3:5] + "-" + str[0:2]) #la ristrutturo nel formato corretto
+                #print(str[6:11]+ "-" + str[3:5] + "-" + str[0:2]) #la ristrutturo nel formato corretto
+                str2 = (str[6:11] + "-" + str[3:5] + "-" + str[0:2])
+                riga[0] = str2
+                writer.writerow(riga[:])
             else:
                 print(f"Scrivo la riga: {riga[:]}")
                 writer.writerow(riga[:]) #In questo caso la riga è formattata come vogliamo e la copiamo nel nuovo csv
