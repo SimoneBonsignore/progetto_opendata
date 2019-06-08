9b08aec14030d0c7cf21525a83d686ef06155548
simone <bonsisimone94@gmail.com>
2019-06-08 17:19:27 +0200

    Ristruttura codice per palermo adeguandolo alle altre tabelle

--- palermo.py
+++ palermo.py
@@ -3,12 +3,12 @@ import csv
 import re
 import datetime
 
-with open("./dati/sinistri2018.csv", newline="", encoding="ISO-8859-1") as sinistri2018: #Apro il csv d'origine
-    with open('./dati/newSinistri.csv', 'w', newline="") as newSinistri: #Creo il nuovo csv in modalità scrittura
-        lettore = csv.reader(sinistri2018, delimiter=";") 
+with open("./datiOriginali/palermo.csv", newline="", encoding="ISO-8859-1") as palermo: #Apro il csv d'origine
+    with open('./datiElaborati/newPalermo.csv', 'w', newline="") as newPalermo: #Creo il nuovo csv in modalità scrittura
+        lettore = csv.reader(palermo, delimiter=";") 
         header = next(lettore) 
-        writer = csv.writer(newSinistri, delimiter=";") 
-        writer.writerow(header[:]) #Copio l'header del file di origine nel nuovo file
+        writer = csv.writer(newPalermo, delimiter=";") 
+        writer.writerow(["Citta'", "Data", "Ora", "Luogo", "Latitudine", "Longitudine", "Illesi", "Feriti", "Riservata", "Decessi", "Natura"])
         print(header)
         dati = [(linea[:]) for linea in lettore]
         for riga in dati:
@@ -21,6 +21,6 @@ with open("./dati/sinistri2018.csv", newline="", encoding="ISO-8859-1") as sinis
                     riga[0] = datetime.datetime.strptime(riga[0], "%d/%m/%Y").strftime("%Y-%m-%d") #converte le date nel formato corretto
 
                 print(f"Scrivo la riga: {riga[:]}")
-                writer.writerow(riga[:]) #In questo caso la riga è formattata come vogliamo e la copiamo nel nuovo csv
+                writer.writerow(["Palermo",riga[0], riga[1], riga[2], riga[5], riga[6], "", riga[4], "","",""]) #In questo caso la riga è formattata come vogliamo e la copiamo nel nuovo csv
 
     
